c0ad695c6332992082765ea72ca27542b0e6b643
will <willhargrave@hotmail.co.uk>
2023-04-24 15:15:21 +0900

    home

--- app/home.js
+++ app/home.js
@@ -8,26 +8,28 @@ import Welcome from "../components"
 const Home = () => {
     const router = useRouter();
     return (
-        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
-          <Stack.Screen
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen
           options={{
             headerStyle: { backgroundColor: COLORS.lightWhite },
             headerShadowVisible: false,
             headerLeft: () => (
-                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+              <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
             ),
             headerRight: () => (
-                <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+              <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
             ),
             headerTitle: "",
           }}
-          />
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View  
+        />
+  
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View
             style={{
-                    flex: 1,
-                    padding: SIZES.medium,
-                }}>
+              flex: 1,
+              padding: SIZES.medium,
+            }}
+          >
                <Welcome 
                
                />
