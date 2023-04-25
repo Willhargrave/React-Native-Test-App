f1a560b27adc598490e07b091d66f8acf5aa7349
will <willhargrave@hotmail.co.uk>
2023-04-25 13:51:47 +0900

    API

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
-const useFetch = () => {
+import {RAPID_API_KEY} from '@env';
+const rapidApiKey = RAPID_API_KEY
+const useFetch = (endpoint) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 }
+
+const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+        'content-type': 'application/octet-stream',
+        'X-RapidAPI-Key': rapidApiKey,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      },
+    params: {
+      query: 'Python developer in Texas, USA',
+      page: '1',
+      num_pages: '1'
+    },
+
+  };
