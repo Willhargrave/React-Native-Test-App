b0f07b209e2cba3b4c10adb4a1bf575d5941b4bf
will <willhargrave@hotmail.co.uk>
2023-04-25 13:56:43 +0900

    errors

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -2,11 +2,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import {RAPID_API_KEY} from '@env';
 const rapidApiKey = RAPID_API_KEY
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-}
 
 const options = {
     method: 'GET',
@@ -16,10 +15,24 @@ const options = {
         'X-RapidAPI-Key': rapidApiKey,
         'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
       },
-    params: {
-      query: 'Python developer in Texas, USA',
-      page: '1',
-      num_pages: '1'
-    },
+    params: {...query},
 
   };
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('error')
+    } finally {
+      setIsLoading(false);
+    } 
+  }
+useEffect(() => {
+    fetchData();
+  }, []);
+}
