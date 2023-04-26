b146a9e7af5e53cedb29ad5a33a9c6f477b20046
will <willhargrave@hotmail.co.uk>
2023-04-26 13:52:45 +0900

    Company

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import styles from './company.style'
-
-const Company = () => {
+import {icons} from '../../../constants';
+import { checkImageURL } from '../../../utils';
+const Company = (companyLogo, jobTitle, companyName, location) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image 
+        source={{
+          url: checkImageURL(companyLogo)
+          ? companyLogo 
+          : "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"
+        }}
+        style={styles.logoImage}
+        />
+      </View>
+      <View>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
     </View>
   )
 }
