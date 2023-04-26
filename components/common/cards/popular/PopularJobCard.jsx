70ab7479bd7e9fe4d9d9c58e7142bf490cdf25eb
will <willhargrave@hotmail.co.uk>
2023-04-26 21:19:18 +0900

    cards

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
-
 import { checkImageURL } from '../../../../utils';
+
 const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
   return (
     <TouchableOpacity 
     style={styles.container(selectedJob, item)}
-    onPress={() => handleCardPress(item)}>
+    onPress={() => handleCardPress(item)}
+    >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image 
-        source={{uri: checkImageURL(item.employer_logo)
+        source={{uri: checkImageURL(item?.employer_logo)
         ? item.employer_logo :
         'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'}}
         resizeMode='contain'
@@ -27,4 +27,4 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
   )
 }
 
-export default PopularJobCard
+export default PopularJobCard;
