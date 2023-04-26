65bff8a8f67c725d609d5f195dbed2f506530d2c
will <willhargrave@hotmail.co.uk>
2023-04-26 15:11:23 +0900

    search home

--- app/home.js
+++ app/home.js
@@ -6,6 +6,7 @@ import { COLORS, icons, images, SIZES } from "../constants"
 import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from "../components"
 const Home = () => {
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("");
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
         <Stack.Screen
@@ -30,7 +31,13 @@ const Home = () => {
             }}
           >
                <Welcome 
-               
+               searchTerm={searchTerm}
+               setSearchTerm={setSearchTerm}
+               handleClick={() => {
+                if(searchTerm) {
+                  router.push(`/search${searchTerm}`)
+                }
+               }}
                />
                <Popularjobs />
             <Nearbyjobs />
